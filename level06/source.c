020665d7491cc0023fa596d6447d121d3a9606e2
Robin Baraud <rbaraud@yahoo.fr>
2020-09-08 13:54:38 +0200

    Corrects details in level06/source.c and adds another method into the writeup

--- level06/source.c
+++ level06/source.c
@@ -9,37 +9,37 @@ int auth(char *login, unsigned int serial)
     size_t          i;
     unsigned int    key;
     size_t          len;
-	unsigned long	tmp;
+    unsigned long   tmp;
 
     login[strcspn(login, "\n")] = '\0';
     len = strnlen(login, 32);
     if (len <= 5)
-        return (0);
+        return (1);
     if (ptrace(PTRACE_TRACEME, 0, 1, 0) == -1)
     {
         puts("\x1b[32m.---------------------------.");
         puts("\x1b[31m| !! TAMPERING DETECTED !!  |");
         puts("\x1b[32m'---------------------------'");
-        return (0);
+        return (1);
     }
     key = (login[3] ^ 0x1337) + 0x5eeded;
     i = 0;
     while (i < len)
     {
-        if (!isprint(login[i]))
-            return (0);
+        if (login[i] < 0x1f)
+            return (1);
         tmp = (login[i] ^ key) * 0x88233b2b;
-		tmp >>= 32;
-		tmp = (((key - tmp) >> 1) + tmp) >> 10;
+        tmp >>= 32;
+        tmp = (((key - tmp) >> 1) + tmp) >> 10;
         tmp = (signed long)tmp * 0x539;
-		tmp = key - (tmp & 0x00000000ffffffff);
-		key += tmp;
-		i++;
+        tmp = key - (tmp & 0x00000000ffffffff);
+        key += tmp;
+        i++;
     }
-	printf("%d", key);
+    //printf("%d", key);
     if (serial == key)
-        return (1);
-    return (0);
+        return (0);
+    return (1);
 }
 
 int main()
@@ -61,7 +61,7 @@ int main()
     printf("-> Enter Serial: ");
     scanf("%u", &serial);
 
-    if (!auth(login, serial))
+    if (auth(login, serial))
         return (1);
     puts("Authenticated!");
     system("/bin/sh");
